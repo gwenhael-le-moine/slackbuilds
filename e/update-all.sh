1efbe3ebdf7a838e6c0a9194b43810f6208fc0d1
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2010-11-16 17:12:34 +0700

    update

--- e/update-all.sh
+++ e/update-all.sh
@@ -7,10 +7,11 @@ upgradepkg /tmp/emacs-trunk*.t?z
 mv /tmp/emacs-trunk*.t?z /home/installs/PKGs/
 
 for e in $(find . -type d -maxdepth 1 -not -name UNUSED -not -name . -not -name emacs); do
-   (cd $e
-      ./$(basename $e).SlackBuild && \
-         upgradepkg /tmp/$(basename $e)*.t?z && \
-         mv /tmp/$(basename $e)*.t?z /home/installs/PKGs/
+    (cd $e
+        THIS=$(basename $e)
+        sh $THIS.SlackBuild \
+            && upgradepkg /tmp/$THIS*.t?z \
+            && mv /tmp/$THIS*.t?z /home/installs/PKGs/
     )
 done
 
