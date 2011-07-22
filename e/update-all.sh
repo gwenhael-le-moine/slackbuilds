bde4e1baedb0d22a35fe2f5899d60e7c08cf0c3b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2011-07-22 11:36:06 +0200

    move bits around

--- e/update-all.sh
+++ e/update-all.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 ( cd emacs
-	./emacs.SlackBuild
+	./emacs.SlackBuild \
+		&& upgradepkg /tmp/emacs-trunk*.t?z \
+		&& mv /tmp/emacs-trunk*.t?z /home/installs/PKGs/
 )
-upgradepkg /tmp/emacs-trunk*.t?z
-mv /tmp/emacs-trunk*.t?z /home/installs/PKGs/
 
-for e in $(find . -type d -maxdepth 1 -not -name UNUSED -not -name . -not -name emacs); do
-    (cd $e
-        THIS=$(basename $e)
+for i in $(find . -type d -maxdepth 1 -not -name UNUSED -not -name . -not -name emacs); do
+    (cd $i
+        THIS=$(basename $i)
         sh $THIS.SlackBuild \
             && upgradepkg --install-new /tmp/$THIS*.t?z \
             && mv /tmp/$THIS*.t?z /home/installs/PKGs/
