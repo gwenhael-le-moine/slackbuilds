9d356aabf0e2388cb0ac35e998ee1b4131dee8d8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2011-07-23 13:00:50 +0200

    sometimes I don't want to compile a new Emacs

--- e/update-all.sh
+++ e/update-all.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-( cd emacs
-	./emacs.SlackBuild \
-		&& upgradepkg /tmp/emacs-trunk*.t?z \
-		&& mv /tmp/emacs-trunk*.t?z /home/installs/PKGs/
-)
+if [ ! "x$1" == "x--not-emacs" ]; then
+	( cd emacs
+		./emacs.SlackBuild \
+			&& upgradepkg /tmp/emacs-trunk*.t?z \
+			&& mv /tmp/emacs-trunk*.t?z /home/installs/PKGs/
+	)
+fi
 
 for i in $(find . -type d -maxdepth 1 -not -name UNUSED -not -name . -not -name emacs); do
     (cd $i
