854cb30a484f63113299015be45ab990531dfb8c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2012-12-12 13:46:06 +0100

    add the_silver_searcher a.k.a. ag, a replacement for ack and grep
    Signed-off-by: Gwenhael Le Moine <gwenhael.le.moine@gmail.com>

--- xap/update-google_chrome.sh
+++ xap/update-google_chrome.sh
@@ -15,7 +15,8 @@ CHANNEL=${CHANNEL:=stable}
 
 cd $BUILD_DIR
 [ -e google-chrome-${CHANNEL}_current_$ARCH.deb ] && rm google-chrome-${CHANNEL}_current_$ARCH.deb
-wget -c http://dl.google.com/linux/direct/google-chrome-${CHANNEL}_current_$ARCH.deb
+wget -c --no-check-certificate https://dl.google.com/linux/direct/google-chrome-${CHANNEL}_current_$ARCH.deb
+
 RELEASE=$CHANNEL ./google-chrome.SlackBuild
 
 [ "x$1" == "x--install" ] && upgradepkg --install-new /tmp/google-chrome-*-$ARCH-*.txz
