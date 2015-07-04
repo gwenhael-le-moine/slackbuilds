961f61dc1538fb0929f978d3136974bf5828a27d
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-07-04 13:30:57 +0200

    headless

--- xap/update-google_chrome.sh
+++ xap/update-google_chrome.sh
@@ -5,18 +5,20 @@ BUILD_DIR=${BUILD_DIR:=/home/installs/mirrors/slackware64-current/extra/google-c
 ARCH=${ARCH:=$(uname -m)}
 case $ARCH in
     "i?86")
-        ARCH=i386
-        ;;
+	DEBARCH=i386
+	;;
     "x86_64")
-        ARCH=amd64
-        ;;
+	DEBARCH=amd64
+	;;
 esac
 CHANNEL=${CHANNEL:=stable}
 
 cd $BUILD_DIR
-[ -e google-chrome-${CHANNEL}_current_$ARCH.deb ] && rm google-chrome-${CHANNEL}_current_$ARCH.deb
-wget -c --no-check-certificate https://dl.google.com/linux/direct/google-chrome-${CHANNEL}_current_$ARCH.deb
+[ -e google-chrome-${CHANNEL}_current_$ARCH.deb ] && rm google-chrome-${CHANNEL}_current_$DEBARCH.deb
+wget -c --no-check-certificate https://dl.google.com/linux/direct/google-chrome-${CHANNEL}_current_$DEBARCH.deb
 
 RELEASE=$CHANNEL ./google-chrome.SlackBuild
 
-[ "x$1" == "x--install" ] && upgradepkg --install-new /tmp/google-chrome-*-$ARCH-*.txz
+upgradepkg --install-new /tmp/google-chrome-*-$ARCH-*.txz
+
+rm google-chrome-${CHANNEL}_current_$DEBARCH.deb
