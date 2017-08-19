1a30f492cbf89bb49d2ab4f032640f24391763dc
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-08-19 15:47:54 +0200

    better flash plugin updater

--- xap/flashplayer-plugin.sh
+++ xap/flashplayer-plugin.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-LATEST_VERSION=$(wget -c "https://get.adobe.com/fr/flashplayer/download/?installer=Flash_Player_11.2_for_other_Linux_(.tar.gz)_64-bit&standalone=1" -O - | grep -o "https://fpdownload.adobe.com/get/flashplayer/pdc/[0-9.]*/install_flash_player_11_linux.x86_64.tar.gz" | grep -o "/[0-9.]*/" | tr -d / | grep -v "^$")
-
-cd /home/installs/mirrors/slackware64-current/extra/flashplayer-plugin
-VERSION=$LATEST_VERSION ./flashplayer-plugin.SlackBuild
-rm install_flash_player*.tar.gz
+cp -a /home/installs/mirrors/slackware64-current/extra/flashplayer-plugin/ /tmp/
+( cd /tmp/flashplayer-plugin
+  ./flashplayer-plugin.SlackBuild
+)
+rm -r /tmp/flashplayer-plugin
 
 upgradepkg --install-new --reinstall /tmp/flashplayer-plugin-*.txz
 /root/clean-tmp.sh
