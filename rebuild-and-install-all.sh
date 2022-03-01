b0294e1fde5d8704ae6d7377f709690403c5f447
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2022-03-01 15:25:33 +0100

    -e
    Signed-off-by: Gwenhael Le Moine <gwenhael.le.moine@gmail.com>

--- rebuild-and-install-all.sh
+++ rebuild-and-install-all.sh
@@ -2,10 +2,12 @@
 
 WD=/home/installs/SlackBuilds
 CAT=${CAT:-$1}
+
+cd $WD
+./what-s_installed.sh | grep -v "^❌.*" | cut -d\  -f2 | grep "^$CAT.*"
+
 for p in $(./what-s_installed.sh | grep -v "^❌.*" | cut -d\  -f2 | grep "^$CAT.*"); do
     cd $p
-    echo -n "$p : "
-    ls
-    ./SlackBuild
+    bash -e ./SlackBuild
     cd $WD
 done
