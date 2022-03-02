57e95eb8dd779864c421d06a4dd3829ca8cc8224
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2022-03-02 21:29:57 +0100

    [what-s_installed.sh] fixed

--- what-s_installed.sh
+++ what-s_installed.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PKGs=$(ls /var/adm/packages/ | sed 's|^\(.*\)-.*-.*-.*$|\1|')
+
 for cat in a ap d l lua n wayland xap y; do
     cd $cat
     for p in $(ls -1); do
-	ls /var/adm/packages/ | grep -q $(echo $p | tr -d /) && echo -n "✓: " || echo -n "❌: "
+	echo $PKGs | grep -q "\ $(echo $p | tr -d /)\ " && echo -n "✓: " || echo -n "❌: "
 	echo "$cat/$p"
     done | sort
     cd ..
