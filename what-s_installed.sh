de9c6302b0d97cd42c1daea04e81be4418b641f4
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2023-03-06 10:42:03 +0100

    fix trailing \

--- what-s_installed.sh
+++ what-s_installed.sh
@@ -5,7 +5,7 @@ PKGs=$(ls /var/adm/packages/ | sed 's|^\(.*\)-.*-.*-.*$|\1|')
 for cat in a ap d l lua n wayland xap y; do
     cd $cat
     for p in $(ls -1); do
-	echo $PKGs | grep -q "\ $(echo $p | tr -d /)\ " && echo -n "✓: " || echo -n "❌: "
+	echo $PKGs | grep -q " $(echo $p | tr -d /) " && echo -n "✓: " || echo -n "❌: "
 	echo "$cat/$p"
     done | sort
     cd ..
