6154e76eb608ca8fe3c3036a63292a6cce5bee7d
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2023-03-24 13:08:00 +0100

    add confirmation step

--- rebuild-and-install-all.sh
+++ rebuild-and-install-all.sh
@@ -6,6 +6,9 @@ CAT=${CAT:-$1}
 cd $WD
 ./what-s_installed.sh | grep -v "^❌.*" | cut -d\  -f2 | grep "^${CAT}/.*"
 
+echo "Ctrl-C to stop here, enter to continue"
+read
+
 for p in $(./what-s_installed.sh | grep -v "^❌.*" | cut -d\  -f2 | grep "^${CAT}/.*"); do
     (cd $p
      bash -e ./SlackBuild || echo "ERROR $p" >> $WD/_errors_rebuild)
