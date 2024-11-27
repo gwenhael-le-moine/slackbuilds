42c4947468caace20532d4876576af6ba474eb12
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2024-11-27 10:40:55 +0100

    ++

--- what-s_installed.sh
+++ what-s_installed.sh
@@ -4,11 +4,14 @@ cd $(dirname $0) || exit 1
 
 PKGs=$(ls /var/lib/pkgtools/packages/ | sed 's|^\(.*\)-.*-.*-.*$|\1|')
 
+PKG_OK="| x "
+PKG_KO="|   "
+
 for cat in a ap d fonts l n xap y; do
     cd $cat
     for p in $(ls -1); do
-        echo $PKGs | grep -q " $(echo $p | tr -d /) " && echo -n "✓: " || echo -n "❌: "
-        echo "$cat/$p"
+        echo $PKGs | grep -q " $(echo $p | tr -d /) " && echo -n "$PKG_OK" || echo -n "$PKG_KO"
+        echo "| $cat/$p"
     done | sort
     cd ..
 done
