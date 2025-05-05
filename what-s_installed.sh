a67007a890d5aa4df4ebe63ccb5b9342c71177a6
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2025-05-05 18:39:40 +0200

    only check in gwh's installed packages

--- what-s_installed.sh
+++ what-s_installed.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd $(dirname $0) || exit 1
+cd $(dirname "$0") || exit 1
 
-PKGs=$(ls /var/lib/pkgtools/packages/ | sed 's|^\(.*\)-.*-.*-.*$|\1|')
+PKGs=$(ls /var/lib/pkgtools/packages/ | grep "gwh$" | sed 's|^\(.*\)-.*-.*-.*$|\1|')
 
 PKG_OK="| x "
 PKG_KO="|   "
 
 cats=${1:-a ap d fonts l n xap y}
 for cat in $cats; do
-    cd $cat
+    cd "$cat" || exit 1
     for p in $(ls -1); do
         echo $PKGs | grep -q " $(echo $p | tr -d /) " && echo -n "$PKG_OK" || echo -n "$PKG_KO"
         echo "| $cat/$p"
