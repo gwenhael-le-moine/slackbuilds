bf126c82155f3e3391e4832eb5d35c7944d495f5
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2025-05-05 18:40:29 +0200

    make-gwh-world.sh check on installed packages instead of hardcoded list

--- make-gwh-world.bash
+++ make-gwh-world.bash
@@ -3,78 +3,82 @@
 cd "$(dirname "$0")" || exit 1
 CWD=$(pwd)
 
-for pkg in \
-    a/nct6687d \
-        ap/aegis-rs \
-        ap/bat \
-        ap/btop \
-        ap/checkssl \
-        ap/conty-bin \
-        ap/delta \
-        ap/fd \
-        ap/getssl \
-        ap/glow \
-        ap/gopsuinfo \
-        ap/hledger-bin \
-        ap/jellyfin-tui \
-        ap/just \
-        ap/ledger \
-        ap/libtree \
-        ap/lsd \
-        ap/ncdu \
-        ap/nvtop \
-        ap/pastel \
-        ap/pandoc-bin \
-        ap/ripgrep \
-        ap/rkvm \
-        ap/starship \
-        ap/tinty \
-        ap/vivid \
-        ap/zauth \
-        d/crystal-lang \
-        d/factor-lang \
-        d/luarocks \
-        d/rpn-lang \
-        d/uxn \
-        d/zig-lang-bin \
-        e/emacs \
-        l/fcft \
-        l/libscfg \
-        l/notcurses \
-        l/spdlog \
-        l/tllist \
-        n/forgejo-bin \
-        n/jellyfin-bin \
-        xap/brightnessctl \
-        xap/cmd-polkit \
-        xap/dunst \
-        xap/foot \
-        xap/freetube \
-        xap/fuzzel \
-        xap/fuzzel-polkit-agent \
-        xap/gammastep \
-        xap/grim \
-        xap/guile-swayer \
-        xap/hpemung \
-        xap/i3status-rust \
-        xap/nextcloud-desktop \
-        xap/openrgb \
-        xap/saturnng \
-        xap/slurp \
-        xap/swappy \
-        xap/sway \
-        xap/swayidle \
-        xap/wbg \
-        xap/wlopm \
-        xap/wlr-randr \
-        xap/x48ng \
-        xap/x50ng \
-        y/csol \
-        y/solitaire-tui \
-    ;
-do
-    echo -n "$pkg > "
-    PKGNAM="$(PRINT_PACKAGE_NAME=yes "$CWD"/make-pkg.bash "$pkg" | tail -n1 2>&1)"
+# a/nct6687d \
+    #     ap/aegis-rs \
+    #     ap/bat \
+    #     ap/btop \
+    #     ap/checkssl \
+    #     ap/conty-bin \
+    #     ap/delta \
+    #     ap/fd \
+    #     ap/getssl \
+    #     ap/glow \
+    #     ap/gopsuinfo \
+    #     ap/hledger-bin \
+    #     ap/jellyfin-tui \
+    #     ap/just \
+    #     ap/ledger \
+    #     ap/libtree \
+    #     ap/lsd \
+    #     ap/ncdu \
+    #     ap/nvtop \
+    #     ap/pastel \
+    #     ap/pandoc-bin \
+    #     ap/ripgrep \
+    #     ap/rkvm \
+    #     ap/starship \
+    #     ap/tinty \
+    #     ap/vivid \
+    #     ap/zauth \
+    #     d/crystal-lang \
+    #     d/factor-lang \
+    #     d/luarocks \
+    #     d/rpn-lang \
+    #     d/uxn \
+    #     d/zig-lang-bin \
+    #     e/emacs \
+    #     l/fcft \
+    #     l/libscfg \
+    #     l/notcurses \
+    #     l/spdlog \
+    #     l/tllist \
+    #     n/forgejo-bin \
+    #     n/jellyfin-bin \
+    #     xap/brightnessctl \
+    #     xap/cmd-polkit \
+    #     xap/dunst \
+    #     xap/foot \
+    #     xap/freetube \
+    #     xap/fuzzel \
+    #     xap/fuzzel-polkit-agent \
+    #     xap/gammastep \
+    #     xap/grim \
+    #     xap/guile-swayer \
+    #     xap/hpemung \
+    #     xap/i3status-rust \
+    #     xap/nextcloud-desktop \
+    #     xap/openrgb \
+    #     xap/saturnng \
+    #     xap/slurp \
+    #     xap/swappy \
+    #     xap/sway \
+    #     xap/swayidle \
+    #     xap/wbg \
+    #     xap/wlopm \
+    #     xap/wlr-randr \
+    #     xap/x48ng \
+    #     xap/x50ng \
+    #     y/csol \
+    #     y/solitaire-tui \
+
+for pkg in $("$CWD"/what-s_installed.sh | grep "^| x | " | sed 's/| x | //') ;
+    do
+        echo -n "$pkg > "
+        if ! grep -q "PRINT_PACKAGE_NAME" "$pkg"/SlackBuild; then
+            echo "INCOMPATIBLE"
+            continue
+        fi
+        PKGNAM="$(PRINT_PACKAGE_NAME=yes "$CWD"/make-pkg.bash "$pkg" | tail -n1 2>&1)"
 
     if [ -e /home/installs/PKGs/x86_64/gwh/"$PKGNAM" ]; then
         echo "✅"
